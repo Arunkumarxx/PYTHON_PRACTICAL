c32d52e6cd773abf3ced3063b71fe0346a8bb44e
Arun  kumar <107796370+Arun-Kumar2004@users.noreply.github.com>
2023-04-20 10:22:52 +0530

    Create PERFORM CLASS AND METHODS.py

---
+++ PERFORM CLASS AND METHODS.py
@@ -0,0 +1,13 @@
+# Program coding:
+class Room:
+    length=0.0
+    breadth=0.0
+    def calculate_area(Self):
+        print("Area of Room = ",Self.length*Self.breadth)
+study_room=Room()
+study_room.length=42.5
+study_room.breadth=35.8
+study_room.calculate_area()
+
+# Output:
+# ('Area of Room = ', 1521.4999)
