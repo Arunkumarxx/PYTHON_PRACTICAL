beed856f25008c648a33d238bb1569b1b47f45e7
Arun  kumar <107796370+Arun-Kumar2004@users.noreply.github.com>
2023-04-20 10:31:36 +0530

    Create ENCAPSULATION.py

---
+++ ENCAPSULATION.py
@@ -0,0 +1,25 @@
+# Program coding:
+class Base1:  
+    def __init__(self):  
+        # the protected member  
+        self._p = 78  
+class Derived1(Base1):  
+    def __init__(self):
+        Base1.__init__(self)  
+        print ("We will call the protected member of base class: ",self._p)  
+        self._p = 433  
+        print ("we will call the modified protected member outside the class: ",self._p)  
+obj_1 = Derived1()   
+obj_2 = Base1()    
+print ("Access the protected member of obj_1: ", obj_1._p)   
+print ("Access the protected member of obj_2: ", obj_2._p)  
+
+# Output:
+
+# ('We will call the protected member of base class: ', 78)
+
+# ('we will call the modified protected member outside the class: ', 433)
+
+# ('Access the protected member of obj_1: ', 433)
+
+# ('Access the protected member of obj_2: ', 78)
